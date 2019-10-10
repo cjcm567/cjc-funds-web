2ca861eacc4842e2c59c761d7053d3bc49056818
Adam Du <adam.du@cjcmarkets.com>
2019-10-10 15:40:47 +1300

    :sparkles: Added highcharts.
    Added highcharts.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,7 @@
 /** @format */
 
+// eslint-disable-next-line no-dupe-keys
+
 import React, {Suspense} from "react"
 import {Link} from "react-router-dom"
 import logo from "../images/logoicon.png"
@@ -9,9 +11,31 @@ function Footer() {
         window.scrollTo(0, 0)
         return null
     }
+    const bannerBgStyle = {
+        color: "#ffffff",
+        // background: "rgb(0,98,204)",
+        // background: "-moz-linear-gradient(90deg, rgba(0,98,204,1) 0%, rgba(39,141,202,1) 50%);",
+        // background: "-webkit-linear-gradient(90deg, rgba(0,98,204,1) 0%, rgba(39,141,202,1) 50%)",
+        background: "linear-gradient(90deg, rgba(0,98,204,1) 0%, rgba(39,141,202,1) 50%)",
+        filter:
+            "progid:DXImageTransform.Microsoft.gradient(startColorstr='#0062cc',endColorstr='#278dca',GradientType=1)",
+        fontSize: "30px",
+    }
     const copyrightYear = new Date().getFullYear()
     return (
         <>
+            <div className="py-4" style={bannerBgStyle}>
+                <div className="row">
+                    <div className="col-lg-8 pl-5 text-center">
+                        <span className="h3">See how we can help. Contact us today</span>
+                    </div>
+                    <div className="col-lg-4 text-center pr-5">
+                        <Link to="/contact-us" className="btn-sm btn-primary" style={{textTransform: "uppercase"}}>
+                            Get In Touch
+                        </Link>
+                    </div>
+                </div>
+            </div>
             <footer className="footer">
                 <div className="py-50 border-bottom" style={{borderColor: "#454547 !important"}}>
                     <div className="container">
@@ -90,30 +114,6 @@ function Footer() {
                                     <code>Carrick Funds</code> &copy; {copyrightYear} All Right Reserved
                                 </p>
                             </div>
-                            {/* <div className="col-lg-6 col-md-7">
-                                <ul className="list-inline text-center text-md-right">
-                                    <li className="list-inline-item mx-lg-3 my-lg-0 mx-2 my-2">
-                                        <a className="font-secondary " href="#">
-                                            Legal
-                                        </a>
-                                    </li>
-                                    <li className="list-inline-item mx-lg-3 my-lg-0 mx-2 my-2">
-                                        <a className="font-secondary " href="#">
-                                            Sitemap
-                                        </a>
-                                    </li>
-                                    <li className="list-inline-item mx-lg-3 my-lg-0 mx-2 my-2">
-                                        <a className="font-secondary " href="#">
-                                            Privacy Policy
-                                        </a>
-                                    </li>
-                                    <li className="list-inline-item ml-lg-3 my-lg-0 ml-2 my-2 ml-0">
-                                        <a className="font-secondary " href="#">
-                                            Terms &amp; Conditions
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div> */}
                         </div>
                     </div>
                     {/* back to top */}
--- src/pages/Manager.tsx
+++ src/pages/Manager.tsx
@@ -1,12 +1,33 @@
 /** @format */
 
-import React from "react"
+import React, {lazy, Suspense} from "react"
 import Layout from "../components/layout"
+import Chart from "../components/Chart"
 import imageProfile from "../images/aboutus/profile.jpg"
+const InvestmentFlowComponent = lazy(() => import("../components/InvestmentFlow"))
+const InvestmentFlow = () => {
+    return (
+        <Suspense fallback="loading...">
+            <InvestmentFlowComponent />
+        </Suspense>
+    )
+}
 
 export default function Manager() {
-    const cardHeightStyle = {
-        minHeight: "36vh",
+    const options: Highcharts.Options = {
+        title: {
+            text: "My chart",
+        },
+        series: [
+            {
+                type: "line",
+                data: [1, 2, 3],
+            },
+        ],
+        credits: {
+            text: "Carrick Just Asset",
+            href: "https://carrickasset.com",
+        },
     }
 
     return (
@@ -63,7 +84,7 @@ export default function Manager() {
                                     <h2>高飞</h2>
                                     <h3 className="text-primary mb-30">基金经理人</h3>
                                     <p className="h5 mb-40">
-                                        高飞作为中国第一代股民，拥有近30年的证券投资交易经验和近 20 年的外汇投资经验。
+                                        高飞作为中国第一代股民，拥有近 30 年的证券投资交易经验和近 20 年的外汇投资经验。
                                     </p>
                                     <div className="row">
                                         {/* contact info */}
@@ -83,6 +104,7 @@ export default function Manager() {
                                             </ul>
                                         </div>
                                     </div>
+                                    <Chart options={options} />
                                     <h4>工作履历</h4>
                                     <p className="h5">曾任</p>
                                     <p>新西兰方圆资产管理有限公司基金经理和策略师</p>
@@ -125,80 +147,7 @@ export default function Manager() {
                         </div>
                     </div>
                 </section>
-                <section className="bg-gray section">
-                    <div className="container">
-                        <div className="row justify-content-center">
-                            <div className="col-lg-12">
-                                <h2 className="section-title">投资流程</h2>
-                            </div>
-                            {/* team member */}
-                            <div className="col-lg-3 col-sm-6 mb-4">
-                                <div className="card">
-                                    <div className="pl-3 pt-3" style={cardHeightStyle}>
-                                        <p>高品质的管理</p>
-                                        <p>竞争优势</p>
-                                        <p>利润和收入增长</p>
-                                        <p>行业顺风</p>
-                                        <p>低传动比</p>
-                                        <p>大量现金转换</p>
-                                        <p>大量的再投资机会</p>
-                                    </div>
-                                    <div className="card-body card-body-2 pb-0 px-4  text-center border-top">
-                                        <h5 className="card-title">寻找符合条件的公司</h5>
-                                        <h6 className="text-color mb-30">第一步</h6>
-                                    </div>
-                                </div>
-                            </div>
-                            {/* team member */}
-                            <div className="col-lg-3 col-sm-6 mb-4">
-                                <div className="card">
-                                    <div className="pl-3 pt-3" style={cardHeightStyle}>
-                                        <p>进行情景分析</p>
-                                        <p>预测财务</p>
-                                        <p>评估业务质量，清单得分和流动性</p>
-                                        <p>将折价/合理估值与同业和历史进行比较</p>
-                                        <p>建立价格目标和排名</p>
-                                        <p>与竞争对手、供应商、客户和其他利益相关者进行渠道检查</p>
-                                    </div>
-                                    <div className="card-body card-body-2 pb-0 px-4 text-center border-top">
-                                        <h5 className="card-title">研究公司具体情况</h5>
-                                        <h6 className="text-color mb-30">第二步</h6>
-                                    </div>
-                                </div>
-                            </div>
-                            {/* team member */}
-                            <div className="col-lg-3 col-sm-6 mb-4">
-                                <div className="card">
-                                    <div className="pl-3 pt-3" style={cardHeightStyle}>
-                                        <p>通过确定信念水平和预期收益进行加权</p>
-                                        <p>无现金头寸限制</p>
-                                        <p>包括对冲货币和市场风险</p>
-                                    </div>
-                                    <div className="card-body card-body-2 pb-0 px-4 text-center border-top">
-                                        <h5 className="card-title">建立投资组合</h5>
-                                        <h6 className="text-color mb-30">第三步</h6>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-3 col-sm-6 mb-4">
-                                <div className="card">
-                                    <div className="pl-3 pt-3" style={cardHeightStyle}>
-                                        <p>审查业务质量以及最近企业公告的含义</p>
-                                        <p>计算收益和投资组合权重</p>
-                                        <p>维护全面的投资组合仪表板</p>
-                                        <p>每两周一次的投资委员会会议</p>
-                                        <p>每周的投资团队会议</p>
-                                        <p>积极监测行业发展</p>
-                                    </div>
-                                    <div className="card-body card-body-2 pb-0 px-4 text-center border-top">
-                                        <h5 className="card-title">评估业务</h5>
-                                        <h6 className="text-color mb-30">第四步</h6>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                <InvestmentFlow />
             </Layout>
         </>
     )
