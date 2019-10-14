9dc62ba0be6048f3636ea54c906bf75f514167e7
Adam Du <adam.du@cjcmarkets.com>
2019-10-15 12:18:06 +1300

    :poop: Shitty code
    Shitty code

--- src/pages/ManagerTeam.tsx
+++ src/pages/ManagerTeam.tsx
@@ -32,19 +32,18 @@ export default function ManagerTeam() {
                 <section className="section">
                     <div className="container">
                         <div className="row justify-content-center">
-                            <div className="col-lg-12">
-                                <h2 className="mb-2">精英团队</h2>
-                                <p className="mb-5">
-                                    经验丰富 理性投资
-                                    <br />
+                            <div className="col-lg-12 mb-50">
+                                <h2 className="mb-5">精英团队</h2>
+                                <h4 className="mb-2">经验丰富 理性投资</h4>
+                                <p className="mb-3">
                                     凯利资本投资研究团队长期并肩从事投资工作，具有丰富的投资经验。团队及个人潜力均得到了的充分发挥。
-                                    <br />
-                                    稳健投资 严控风控
-                                    <br />
+                                </p>
+                                <h4 className="mb-2">稳健投资 严控风控</h4>
+                                <p className="mb-3">
                                     凯利资产管理的理财团队坚信，研究创造价值，对基本面的把握是投资风险控制的基础。在长期投资实践的过程中，凯利资产管理的理财团队形成了一套独具特色、严谨的研究体系，研究面覆盖宏观经济、行业经济及全部的上市公司。任何投资决策均是基于深入研究的基础上作出。
-                                    <br />
-                                    动态追踪 把握先机
-                                    <br />
+                                </p>
+                                <h4 className="mb-2">动态追踪 把握先机</h4>
+                                <p className="mb-3">
                                     在对宏观、行业及上市公司进行深入研究的基础上，凯利资产管理还建立了动态跟踪指标体系，通过把握各种指标的细微变化来发掘投资先机。
                                 </p>
                             </div>
