77521d983cc4b7a97aae3226449681447bf0e89c
Adam Du <adam.du@cjcmarkets.com>
2019-10-21 12:03:56 +1300

    :sparkles: Replace bootstrap carousel with a new slider
    Replace bootstrap carousel with a new slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import React from "react"
+import React, {CSSProperties} from "react"
 import "react-responsive-carousel/lib/styles/carousel.min.css"
 import {Carousel} from "react-responsive-carousel"
 import imageBanner1 from "../images/banner/banner-1.jpg"
@@ -10,28 +10,67 @@ import imageBanner5 from "../images/banner/banner-5.jpg"
 import imageBanner6 from "../images/banner/banner-6.jpg"
 
 export default function Slider() {
+    const legendLeftStyle: CSSProperties = {
+        transition: "all .5s ease-in-out",
+        position: "absolute",
+        bottom: "40%",
+        left: "50%",
+        marginLeft: "-35%",
+        borderRadius: "10px",
+        color: "#fff",
+        padding: "10px",
+        textAlign: "left",
+    }
+    const legendRightStyle: CSSProperties = {
+        transition: "all .5s ease-in-out",
+        position: "absolute",
+        bottom: "40%",
+        left: "50%",
+        marginLeft: "5%",
+        borderRadius: "10px",
+        color: "#fff",
+        padding: "10px",
+        textAlign: "left",
+        fontWeight: "bold",
+    }
+
     return (
         <>
             <Carousel infiniteLoop useKeyboardArrows autoPlay showStatus={false} showThumbs={false}>
-                <div>
-                    <img src={imageBanner2} />
-                    <p className="legend">Legend 1</p>
+                <div className="overlay">
+                    <img src={imageBanner2} height="600px" width="100%" style={{objectFit: "cover"}} />
+                    <div style={legendRightStyle}>
+                        <h1 className="text-white">着眼未来</h1>
+                        <h1 className="text-white">凯利让您看得更远</h1>
+                    </div>
                 </div>
-                <div>
-                    <img src={imageBanner1} />
-                    <p className="legend">Legend 2</p>
+                <div className="overlay">
+                    <img src={imageBanner1} height="600px" width="100%" style={{objectFit: "cover"}} />
+                    <div style={legendLeftStyle}>
+                        <h1 className="text-white">用时间证明价值</h1>
+                        <h1 className="text-white">用收益回报信任</h1>
+                    </div>
                 </div>
-                <div>
-                    <img src={imageBanner3} />
-                    <p className="legend">Legend 3</p>
+                <div className="overlay">
+                    <img src={imageBanner3} height="600px" width="100%" style={{objectFit: "cover"}} />
+                    <div style={legendLeftStyle}>
+                        <h1 className="text-white">因专注</h1>
+                        <h1 className="text-white">而卓越</h1>
+                    </div>
                 </div>
-                <div>
-                    <img src={imageBanner5} />
-                    <p className="legend">Legend 4</p>
+                <div className="overlay">
+                    <img src={imageBanner5} height="600px" width="100%" style={{objectFit: "cover"}} />
+                    <div style={legendRightStyle}>
+                        <h1 className="text-white">智信相伴</h1>
+                        <h1 className="text-white">共赢未来</h1>
+                    </div>
                 </div>
-                <div>
-                    <img src={imageBanner6} />
-                    <p className="legend">Legend 5</p>
+                <div className="overlay">
+                    <img src={imageBanner6} height="600px" width="100%" style={{objectFit: "cover"}} />
+                    <div style={legendLeftStyle}>
+                        <h1 className="text-white">专业创造财富</h1>
+                        <h1 className="text-white">诚信开启未来</h1>
+                    </div>
                 </div>
             </Carousel>
         </>
